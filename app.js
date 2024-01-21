a6a55fed2a6789dac52a853ae70321be6c7bb7b8
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-01-21 15:45:39 -0600

    Estableci una conexión con mi base de datos postgres usando el  ORM Sequelize para mayor protección y seguridad de mi database

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 import express from 'express';
-import { config } from 'dotenv';
-
-config();
+import sequelize from './src/database/config.js';
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.listen(PORT);
+app.listen(PORT , () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+sequelize.authenticate().then(() => {
+    console.log('Connection established successfully.');
+}).catch((error) => {
+    console.error('Unable to connect to the database:', error);
+});
