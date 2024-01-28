f59974b25d740a00a45abd75ed26b14a97641250
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-01-27 19:36:18 -0600

    Refactor code and update dependencies

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 import express from 'express';
-import sequelize from './src/database/config.js';
+import { database } from './src/database/database.js';
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
-
-app.listen(PORT , () => {
-    console.log(`Server running on port ${PORT}`);
-});
-
-sequelize.authenticate().then(() => {
-    console.log('Connection established successfully.');
-}).catch((error) => {
-    console.error('Unable to connect to the database:', error);
-});
+app.listen(PORT);
+database();
