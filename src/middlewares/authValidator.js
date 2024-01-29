da46048801a9c0966562c45235d140b43e30e7e0
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-01-29 11:38:36 -0600

    Add express-validator dependency

---
+++ src/middlewares/authValidator.js
@@ -0,0 +1,56 @@
+import { body } from 'express-validator';
+
+export const registerUserValidations = [
+    body('firstname')
+        .trim()
+        .not()
+        .isEmpty()
+        .withMessage('Name is required')
+        .isLength({ min: 3 })
+        .withMessage('Name must be at least 3 characters long')
+        .matches(/^[a-z]+(\s[a-z]+)?$/i)
+        .withMessage('Name must only contain letters and a single space between words'),
+    body('lastname')
+        .trim()
+        .not()
+        .isEmpty()
+        .withMessage('Lastname is required')
+        .isLength({ min: 3 })
+        .withMessage('Lastname must be at least 3 characters long')
+        .matches(/^[a-z]+(\s[a-z]+)?$/i)
+        .withMessage('Lastname must only contain letters and a single space between words'),
+    body('username')
+        .trim()
+        .not()
+        .isEmpty()
+        .withMessage('Username is required')
+        .isLength({ min: 3 })
+        .withMessage('Username must be at least 3 characters long')
+        .matches(/^[a-zA-Z]+$/)
+        .withMessage('Username must only contain letters'),
+    body('birthdate')
+        .trim()
+        .not()
+        .isEmpty()
+        .withMessage('Birthdate is required'),
+    body('email')
+        .trim()
+        .not()
+        .isEmpty()
+        .withMessage('Email is required')
+        .normalizeEmail()
+        .isEmail()
+        .withMessage('Please enter a valid email'),
+    body('password')
+        .trim()
+        .not()
+        .isEmpty()
+        .withMessage('Password is required')
+        .isLength({ min: 8 })
+        .withMessage('Password must be at least 8 characters long')
+        .matches(/^(?=.*[A-Z])(?=.*\d)/)
+        .withMessage('Password must contain at least one uppercase letter and one number')
+        .not()
+        .contains(' ')
+        .withMessage('Password must not contain spaces'),
+]
