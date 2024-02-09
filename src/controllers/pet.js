6aabac695362640468300628aa8b2007ae0bf243
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-02-09 10:36:18 -0600

    Actualizar controladores de mascotas y usuarios

--- src/controllers/pet.js
+++ src/controllers/pet.js
@@ -2,8 +2,9 @@ import Pet from '../models/pet.js';
 
 export async function createPet(req, res) {
     try {
+        const { name, birthdate, type, breed } = req.body;
         const userId = req.user.id;
-        const pet = await Pet.create({ ...req.body, userId });
+        const pet = await Pet.create({ name, birthdate, type, breed, userId });
         res.status(201).json(pet);
     } catch (error) {
         res.status(500).json({ message: error.message });
