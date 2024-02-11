f1c5e02f29a687f3297fb8c7c5656e329fdb3207
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-02-10 21:01:25 -0600

    Eliminar ruta de obtener mascota por ID

--- src/routes/pet.js
+++ src/routes/pet.js
@@ -1,11 +1,10 @@
 import { Router } from 'express';
-import { createPet, getPet, getPetfromUser } from '../controllers/pet.js';
+import { createPet, getPetfromUser } from '../controllers/pet.js';
 import { authenticateToken } from '../middlewares/authenticateToken.js';
 
 const router = Router();
 
 router.post('/create', authenticateToken, createPet);
 router.get('/pet', authenticateToken, getPetfromUser);
-router.get('/:id', authenticateToken, getPet);
 
 export default router;
