c664c15efacbf992752f53c4c81fddba0ea2f327
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-02-10 21:03:22 -0600

    Agregada ruta para obtener todos los usuarios y mascotas

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createUser, login, getUserandPets } from '../controllers/user.js';
+import { createUser, login, getUserandPets, getAllUsersAndPets } from '../controllers/user.js';
 import { authenticateToken } from '../middlewares/authenticateToken.js';
 
 const router = Router();
@@ -7,5 +7,6 @@ const router = Router();
 router.post('/login', login);
 router.post('/register', createUser);
 router.get('/profile', authenticateToken, getUserandPets);
+router.get('/users', authenticateToken, getAllUsersAndPets);
 
 export default router;
